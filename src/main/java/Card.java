2421a7cc3fe63de3315c96d909a4b6500ffb8910
Hi-Imjaeyoung <imjy2757@naver.com>
2023-08-31 21:13:53 +0900

    feat: Card 클래스내 idcode를 정하여 만 > 통 > 삭 > 동 남 서 북 > 백 발 중 순으로 정렬하도록 구현.

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,17 +1,61 @@
-public class Card {
+public class Card implements Comparable<Card> {
     String type;
     int number;
+    int idCode;
     Card(String type, int number){
         this.type = type;
         this.number = number;
+        calculateIdCode();
     }
     Card(String type){
         this.type = type;
-        this.number =-1;
+        this.number =0;
+        calculateIdCode();
         }
     public String toString(){
-        if(number==-1)
+        if(number==0)
             return type+" ";
         return String.valueOf(number)+type+" ";
+    }
+    private void calculateIdCode(){
+        if(type.equals("MAN")){
+            idCode = number;
+            return;
+        }
+        if(type.equals("Sak")){
+            idCode = number+20;
+            return;
+        }
+        if(type.equals("Tong")){
+            idCode = number+10;
+            return;
+        }
+        if(number==0){
+            if(type.equals("EAST")){
+                idCode=31;
+            }
+            if (type.equals("SOUTH")){
+                idCode=32;
+            }
+            if(type.equals("WEST")){
+                idCode=33;
+            }
+            if(type.equals("NORTH")){
+                idCode=34;
+            }
+            if(type.equals("WHITE")){
+                idCode=35;
+            }
+            if(type.equals("GREEN")){
+                idCode=36;
+            }
+            if(type.equals("RED")){
+                idCode=37;
+            }
+        }
+    }
+    @Override
+    public int compareTo(Card oj) {
+        return this.idCode-oj.idCode;
     }
 }
