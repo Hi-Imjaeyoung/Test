a3c4641a5c30a289729afe629102da36d1c4a486
Hi-Imjaeyoung <imjy2757@naver.com>
2023-08-31 19:56:00 +0900

    Test : Deck의 draw method를 테스트.

--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,12 +1,29 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DeckTest {
     @Test
     void Deck_Initialize_Test(){
         Deck deck = new Deck();
         assertEquals(true,deck.printSize()==136);
+    }
+    @Test
+    void Draw_Test(){
+        //given
+        Deck deck = new Deck();
+        //when
+        Card card = deck.draw();
+        //then
+        if(card.number==-1){
+            assertThat(card.type).containsAnyOf("EAST","SOUTH","NORTH","WEST","GREEN","RED","WHITE");
+        }else{
+            assertThat(card.type).containsAnyOf("MAN","Sak","Tong");
+            assertThat(card.number).isGreaterThan(0)
+                    .isLessThan(10);
+        }
     }
 
 }
