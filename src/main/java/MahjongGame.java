ec82cacb4c627661c1ddfffc39b97581316e33df
Hi-Imjaeyoung <imjy2757@naver.com>
2023-08-31 20:39:27 +0900

    feat: 자신의 13종 패를 String으로 리턴하는 Method를 구현.

--- src/main/java/MahjongGame.java
+++ src/main/java/MahjongGame.java
@@ -1,17 +1,21 @@
 import java.util.*;
 public class MahjongGame {
     Deck deck;
-    List<Card> east_hand,west_hand,north_hand,south_hand;
+    List<Card> east_hand = new ArrayList<>();
     MahjongGame (){
         deck = new Deck();
         settingHand(east_hand);
     }
-    public void settingHand(List<Card> hand){
+    private void settingHand(List<Card> hand){
         while (hand.size()!=13){
             hand.add(deck.draw());
         }
     }
     public String showMyHands(){
-
+        StringBuilder sb = new StringBuilder();
+        for(Card now_card:east_hand){
+            sb.append(now_card.toString());
+        }
+        return sb.toString();
     }
 }
