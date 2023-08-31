fe08717b3f301fffeec8642feda0846718d64aa0
Hi-Imjaeyoung <imjy2757@naver.com>
2023-08-31 17:14:04 +0900

    feat(Deck) : Card 클래스 추가, Deck의 생성자 기능 추가.

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,57 @@
+import java.util.*;
+
 public class Deck {
+    class Card{
+        String type;
+        int number;
+        Card(String type, int number){
+            this.type = type;
+            this.number = number;
+        }
+        Card(String type){
+            this.type = type;
+            this.number =-1;
+        }
+    }
+    static List<Card> cards = new ArrayList<>();
+    public Deck(){
+        for(int i=0;i<4;i++){
+            addManCards();
+            addSakCards();
+            addTongCards();
+            addDragon();
+            addWind();
+        }
+        Collections.shuffle(cards);
+
+    }
+    private  void addManCards(){
+        for(int i=1;i<10;i++){
+            cards.add(new Card("MAN",i));
+        }
+    }
+    private  void addTongCards(){
+        for(int i=1;i<10;i++){
+            cards.add(new Card("Tong",i));
+        }
+    }
+    private  void addSakCards(){
+        for(int i=1;i<10;i++){
+            cards.add(new Card("Sak",i));
+        }
+    }
+    private  void addWind(){
+        cards.add(new Card("EAST"));
+        cards.add(new Card("SOUTH"));
+        cards.add(new Card("WEST"));
+        cards.add(new Card("NORTH"));
+    }
+    private  void addDragon(){
+        cards.add(new Card("WHITE"));
+        cards.add(new Card("GREEN"));
+        cards.add(new Card("RED"));
+    }
+    public int printSize(){
+        return cards.size();
+    }
 }
