9dbc0dec0c3bbc84fd268be9512be9c1ebb05437
Hi-Imjaeyoung <imjy2757@naver.com>
2023-09-19 16:08:34 +0900

    feat : Create user class. set variable , constructor, methods format.

---
+++ src/main/java/User.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class User {
+    //variable
+    List<Card> myCards;
+    int count, block;
+    int[][] cardCounting = new int[3][10];
+    //TODO Don't use hardcoding!
+    boolean[] blocks = new boolean[14];
+
+    //Constructor
+    User(List<Card> list){
+        this.myCards =list;
+        this.count=8;
+        this.block=0;
+        for(int i=0;i<10;i++){
+            Arrays.fill(cardCounting[i],4);
+        }
+        Arrays.fill(blocks,false);
+    }
+
+    //Essential Function
+    public void addCard(Card...cards){
+       for(Card nowcard : cards){
+           myCards.add(nowcard);
+       }
+    }
+    public void deleteTheCard(){
+
+    }
+    public void printUserHand(){
+
+    }
+    public void UpdateCardCounting(){
+
+    }
+    public void sortCards(){
+
+    }
+    public void calculateShentan(){
+
+    }
+    public void checkCompleteBody(){
+
+    }
+    public void checkPair(){
+
+    }
+    public void checkIncompleteBody(){
+
+    }
+
+}
